1acfd88823509aaa6d2586fec320f8d921f3c492
baaaam771 <jeongiun@naver.com>
2020-10-18 02:07:04 +0900

    2.6, 2.7

--- indeed.py
+++ indeed.py
@@ -24,6 +24,28 @@ def extract_indeed_pages():
 
 
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
+    jobs = []
+    # for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    # print(result.status_code)
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    # find_all 은 리스트의 모든 부분
+    # find는 찾은 곳의 첫 부분
+    # print(results)
+    for result in results:
+        # title = (result.find("h2", {"class": "title"}))
+        # anchor = title.find("a")["title"]
+        title = result.find("h2", {"class": "title"}).find("a")["title"]
+        print(title)
+        company = result.find("span", {"class": "company"})
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            # print(str(company.find("a").string))
+            company = str(company.find("a").string)
+        else:
+            # print(str(company.string).strip())
+            company = str(company.string)
+        company = company.strip()
+        print(title, company)
+    return jobs
